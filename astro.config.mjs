501472693f591d7ea0fbd8f7c3508d4a94197264
chiw <wongchiking+github@gmail.com>
2024-07-06 00:32:56 +0800

    add some coffee notes

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,24 +3,39 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+    site: 'https://chiw.github.io',
+    base: 'coffee-notes',
 	integrations: [
 		starlight({
-			title: 'My Docs',
-			social: {
-				github: 'https://github.com/withastro/starlight',
+			favicon: '/assets/misc/favicon/coffee-notes-favicon.svg',
+			title: 'Coffee Notes',
+			logo: {
+				light: './src/assets/logo-dark-100-contrailone.svg',
+				dark: './src/assets/logo-light-100-contrailone.svg',
+				replacesTitle: true,
 			},
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', link: '/guides/example/' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
+			social: {
+				github: 'https://github.com/chiw/coffee-notes',
+			},
+			editLink: {
+				baseUrl: 'https://github.com/chiw/coffee-notes/edit/master',
+			},
+			// sidebar: [
+			// 	{
+			// 		label: 'Guides',
+			// 		items: [
+			// 			// Each item here is one entry in the navigation menu.
+			// 			{ label: 'Example Guide', link: '/guides/example/' },
+			// 		],
+			// 	},
+			// 	{
+			// 		label: 'Reference',
+			// 		autogenerate: { directory: 'reference' },
+			// 	},
+			// ],
+			customCss: [
+				// Relative path to your custom CSS file
+				'./src/styles/custom.css',
 			],
 		}),
 	],
---
+++ src/content/docs/process.md
@@ -0,0 +1,30 @@
+---
+title: 咖啡豆處理法
+description: 咖啡豆處理法
+sidebar:
+  order: 2
+---
+
+### 日曬 (Dry process ∕ Natural method)
+- 處理過程：篩選浮果 → 曝曬 → 脫殼並去除果皮、果肉
+- 風味：酸度和苦味會柔和一些，甜度、果香和酒香較明顯
+
+### 水洗 (Wet process ∕ Washed method)
+- 處理過程：篩選浮果 → 去除果皮、果肉 → 泡水發酵去除果膠 → 清水清洗 → 乾燥 → 脫殼
+- 風味：酸度較強烈，口感乾淨清爽、明亮
+
+### 半水洗 (Semi washed method)
+- 處理過程：篩選浮果 → 去除雜質、果皮和果肉 → 去除果膠 → 乾燥脫殼 → 二次乾燥
+- 風味：與水洗相比甜味較高酸味較低，口感沉穩、醇度豐厚
+
+### 蜜處理 (Honey Process ∕ Miel Process)
+- 處理過程：篩選浮果 → 去除果皮、果肉 → 曝曬 → 脫殼
+- 風味：甜度高酸度低，帶有果香，相較於日曬豆口感更乾淨
+
+### 低溫厭氧發酵 (Anaerobic Fermentation)
+- 處理過程：篩選浮果 → 放入不鏽鋼桶發酵 → 選擇三大處理法中的一種方式乾燥
+- 風味：口感圓潤，帶有淡淡乳酪、奶油或酒釀香氣
+
+### Reference:
+- https://www.twothird.com/Article/Detail/66533
+- https://shop.coffeebox.tw/Article/Detail/72235
