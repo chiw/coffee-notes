75f5748a6c9ffdaf36c763d640637d73df9737ac
chiw <wongchiking+github@gmail.com>
2024-07-06 13:12:17 +0800

    update pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -36,6 +36,8 @@ export default defineConfig({
 			customCss: [
 				// Relative path to your custom CSS file
 				'./src/styles/custom.css',
+				// Supports weights 200-900
+				'@fontsource-variable/noto-serif-tc'
 			],
 		}),
 	],
